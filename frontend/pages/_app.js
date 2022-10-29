cf67432a47b4ae4bf8202f0a390e5c2e80a27295
deependujha <deependujha21@gmail.com>
2022-10-29 08:35:05 +0530

    basic frontend done

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,7 +1,18 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+import styles from "../styles/Home.module.css";
+import { useEffect, useState } from "react";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  const [connected, toggleConnect] = useState(false);
+  const [currAddress, updateAddress] = useState("0x");
+
+  return (
+    <>
+      <div className={`my-20 ${styles.container}`}>
+        <Component {...pageProps} />
+      </div>
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
---
+++ frontend/pages/myProfile.js
@@ -0,0 +1,49 @@
+import React, { useState } from "react";
+import NavBar from "../components/NavBar";
+import NFTTile from "../components/NFTTile";
+
+const myProfile = () => {
+  const [data, updateData] = useState([]);
+  const [address, updateAddress] = useState("0x");
+  const [totalPrice, updateTotalPrice] = useState("0");
+
+  return (
+    <div className="profileClass" style={{ minHeight: "50vh" }}>
+      <NavBar active="profile" />
+
+      <div className="profileClass">
+        <div className="flex text-center flex-col mt-11 md:text-2xl text-white">
+          <div className="mb-5">
+            <h2 className="font-bold">Wallet Address</h2>
+            {address}
+          </div>
+        </div>
+        <div className="flex flex-row text-center justify-center mt-10 md:text-2xl text-white">
+          <div>
+            <h2 className="font-bold">No. of NFTs</h2>
+            {data.length}
+          </div>
+          <div className="ml-20">
+            <h2 className="font-bold">Total Value</h2>
+            {totalPrice} ETH
+          </div>
+        </div>
+        <div className="flex flex-col text-center items-center mt-11 text-white">
+          <h2 className="font-bold">Your NFTs</h2>
+          <div className="flex justify-center flex-wrap max-w-screen-xl">
+            {data.map((value, index) => {
+              return <NFTTile data={value} key={index}></NFTTile>;
+            })}
+          </div>
+          <div className="mt-10 text-xl">
+            {data.length == 0
+              ? "Oops, No NFT data to display (Are you logged in?)"
+              : ""}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default myProfile;
